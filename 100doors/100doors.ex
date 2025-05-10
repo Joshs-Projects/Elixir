3dc3b6941eabc502b2fc62b1964585f630632213
Joshs-Projects <74363804+Joshs-Projects@users.noreply.github.com>
2025-05-10 19:02:07 +0100

    n number of doors and parallelised doors.
    Moved some of the playing with elixir to a chalkboard file.

--- 100doors/100doors.ex
+++ 100doors/100doors.ex
@@ -1,59 +1,8 @@
-number = round(2.345)
-IO.puts("The number is #{number}")
+# To run this file in the terminal, use the command:
+# elixir 100doors.ex and make sure you are in the same directory as the file
 
-list = [1, 2, 3, 4, true, false, "hello", :atom]
-IO.inspect(list)
-
-# Pattern matching
-[a, b] = [1, 2]
-IO.puts("a: #{a}, b: #{b}")
-
-# Pattern matching with lists
-# This will match the first four elements of the list and bind them to a, b, c, d
-list = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, "a", :b, true, :false]
-[a, b, c | tail] = list
-head = [a,b,c]
-IO.puts("Head: #{inspect(head)}, Tail: #{inspect(tail)}")
-
-# Case statement with pattern matching
-case head do
-  [1,2,3] ->
-    IO.puts("Head matches [1,2,3]")
-  [4,5,6] ->
-    IO.puts("Head matches [4,5,6]")
-  _ ->
-    IO.puts("Head does not match [1,2,3]")
-end
-
-# if statement
-if (a == 1) do
-  IO.puts("a is 1")
-else
-  IO.puts("a is not 1")
-end
-
-# cond statement
-cond do
-  a + b == 2 ->
-    IO.puts("a+b is 2")
-  a + b == 3 ->
-    IO.puts("a+b is 3")
-  true ->
-    IO.puts("a is neither 1 nor 2")
-end
-
-defmodule Printer do
-  # Recursive function to print elements
-  # This function takes a list and prints each element then calls itself with the tail of the list
-  def printElements([]), do: :finished # Base case: if the list is empty, return :finished
-  def printElements([head | tail]) do
-    IO.puts("Element: #{head}")
-    printElements(tail)
-  end
-end
-
-# Recursive function to print elements
-Printer.printElements(list)
+# Define the number of doors
+doors = 10000
 
 defmodule HundredDoors do
   # Function to simulate the 100 doors problem
@@ -84,12 +33,56 @@ defmodule HundredDoors do
 
 end
 
+#IO.puts("Sleeping for 120 seconds to allow the cores to cool down")
+#:timer.sleep(120000) # Sleep for 120 second to allow the cores to cool down
+#IO.puts("Waking up now")
+
 # The return value of the doors function is piped to gain an index, then filtered to the doors that are open (true)
 # The result is then mapped to get the door numbers (indexed+1)
-output = HundredDoors.doors(100)
-  |> Enum.with_index()
-  |> Enum.filter(fn {door, _i} -> door == true end)
-  |> Enum.map(fn {_door, i} -> i + 1 end)
+
+# Lets time it now so that we can compare the performance of the two implementations
+{time_microseconds, result} = :timer.tc(fn ->
+  HundredDoors.doors(doors)
+    |> Enum.with_index()
+    |> Enum.filter(fn {door, _i} -> door == true end)
+    |> Enum.map(fn {_door, i} -> i + 1 end)
+  end)
 
 # Print the open doors
-IO.puts("Open doors: #{inspect(output)}")
+IO.puts("Open doors: #{inspect(result)} and took #{time_microseconds/1_000_000} seconds")
+
+#IO.puts("Sleeping for 120 seconds to allow the cores to cool down")
+#:timer.sleep(120000) # Sleep for 120 second to allow the cores to cool down
+#IO.puts("Waking up now")
+
+defmodule HundredDoorsParallel do
+  def doors(n) do
+    # All doors start closed (false)
+    doors = for _ <- 1..n, do: false
+
+    # Use Task.async_stream to parallelize steps 1..n
+    1..n
+    |> Task.async_stream(fn step -> toggled_indices(step, n) end, max_concurrency: System.schedulers_online()) # Parallelize the toggling of doors
+    |> Enum.map(fn {:ok, indices} -> indices end) # Collect the results
+    |> List.flatten() # Flatten the list of lists into a single list
+    |> Enum.frequencies() # Count the number of times each door was toggled
+    |> Enum.reduce(doors, fn {index, count}, doors ->
+      # Toggle the door if it was toggled an odd number of times
+      List.update_at(doors, index, fn val -> rem(count, 2) == 1 and !val or val end) # Update doors at index and if val is odd, toggle it
+    end)
+  end
+
+  # For a given step, return the indices of doors that are toggled
+  defp toggled_indices(step, max) do
+    Enum.filter(0..(max - 1), fn i -> rem(i+1, step) == 0 end) # Return the indices of doors that are toggled
+  end
+end
+
+# Lets time it now so that we can compare the performance of the two implementations
+{time_microseconds, result} = :timer.tc(fn ->
+  HundredDoorsParallel.doors(doors)
+    |> Enum.with_index() # Add index to each door
+    |> Enum.filter(fn {door, _i} -> door == true end) # Filter to get only the open doors
+    |> Enum.map(fn {_door, i} -> i + 1 end) # Map to get the door numbers (indexed+1)
+end)
+IO.puts("Open doors: #{inspect(result)} and took #{time_microseconds/1_000_000} seconds using #{System.schedulers_online()} schedulers")
